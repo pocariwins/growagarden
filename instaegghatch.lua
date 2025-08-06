aecd395f54dbde53b35e4fa603373da8b2fac6fc
pocariwins <pocariisbroke@gmail.com>
2025-08-07 00:20:17 +0800

    Create instaegghatch.lua

---
+++ instaegghatch.lua
@@ -0,0 +1,45 @@
+local TeleportService = game:GetService("TeleportService")
+local Players = game:GetService("Players")
+local CoreGui = game:GetService("CoreGui")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local LocalPlayer = Players.LocalPlayer
+local PlaceId = game.PlaceId
+
+local function loadWait()
+    while not LocalPlayer:GetAttribute("Finished_Loading") do task.wait() end
+    while not LocalPlayer:GetAttribute("DataFullyLoaded") do task.wait() end
+    while not LocalPlayer:GetAttribute("Setup_Finished") do task.wait() end
+    ReplicatedStorage.GameEvents.Finish_Loading:FireServer()
+    ReplicatedStorage.GameEvents.LoadScreenEvent:FireServer(LocalPlayer)
+    while not LocalPlayer:GetAttribute("Loading_Screen_Finished") do task.wait() end
+    task.wait(1)
+end
+
+if LocalPlayer:GetAttribute("Loading_Screen_Finished") then
+    ReplicatedStorage.GameEvents.Finish_Loading:FireServer()
+    ReplicatedStorage.GameEvents.LoadScreenEvent:FireServer(LocalPlayer)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/pocariwins/growagarden/refs/heads/main/important/main.lua"))()
+else
+    loadWait()
+end
+
+local queue = (queueonteleport or queue_on_teleport or syn and syn.queue_on_teleport)
+if queue then
+    queue([[
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/pocariwins/growagarden/refs/heads/main/important/main.lua"))()
+    ]])
+end
+
+CoreGui:WaitForChild("RobloxPromptGui", 10)
+CoreGui.RobloxPromptGui:WaitForChild("promptOverlay", 10).ChildAdded:Connect(function(child)
+    if child.Name == "ErrorPrompt" then
+        task.wait(2)
+        TeleportService:Teleport(PlaceId, LocalPlayer)
+    end
+end)
+
+while true do
+    local startTime = tick()
+    while tick() - startTime < 55 do task.wait(1) end
+    TeleportService:Teleport(PlaceId, LocalPlayer)
+end
